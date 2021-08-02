eb15c40d366b65505abeff8ef41092fea8ae416a
LWQ <2578831517@qq.com>
2021-08-02 21:54:05 +0800

    lombok中的两个坑2

---
+++ src/main/java/com/lhalj/java/escape/lombok_/Computer.java
@@ -0,0 +1,16 @@
+package com.lhalj.java.escape.lombok_;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 描述:
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Computer {
+    private Integer id;
+    private String name;
+}
