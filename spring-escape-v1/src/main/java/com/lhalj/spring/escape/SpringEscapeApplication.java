1771adeb212bfae5501d87041ebe4f1e12399103
2578831517@qq.com <2578831517@qq.com>
2021-08-11 22:02:22 +0800

    springboot项目初始话

---
+++ spring-escape-v1/src/main/java/com/lhalj/spring/escape/SpringEscapeApplication.java
@@ -0,0 +1,13 @@
+package com.lhalj.spring.escape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEscapeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEscapeApplication.class, args);
+    }
+
+}
---
+++ spring-escape-v1/src/test/java/com/lhalj/spring/escape/SpringEscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhalj.spring.escape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEscapeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
